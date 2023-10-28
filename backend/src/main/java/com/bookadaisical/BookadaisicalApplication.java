6ec69012642c1ba760821a7f97d8d9aa1c0f2604
d-Dudas <david.dudas03@e-uvt.ro>
2023-10-28 23:45:44 +0300

    Empty backend added

---
+++ backend/src/main/java/com/bookadaisical/BookadaisicalApplication.java
@@ -0,0 +1,11 @@
+package com.bookadaisical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookadaisicalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookadaisicalApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/bookadaisical/BookadaisicalApplicationTest.java
@@ -0,0 +1,5 @@
+package com.bookadaisical;
+
+public class BookadaisicalApplicationTest {
+
+}
