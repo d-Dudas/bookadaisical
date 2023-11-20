ebf05cd2b58fdefe857bf458dfc0e444f128d69c
Diana Gliga <diana.gliga02@e-uvt.ro>
2023-11-20 12:39:53 +0200

    modified query & added num rows limit

--- backend/src/main/java/com/bookadaisical/repository/BookRepository.java
+++ backend/src/main/java/com/bookadaisical/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.bookadaisical.repository;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,11 +13,11 @@ import com.bookadaisical.model.Book;
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
     
-    @Query("SELECT DISTINCT b FROM Book b JOIN b.activeBooks ab " +
-           "WHERE EXTRACT(MONTH FROM b.createdOn) = EXTRACT(MONTH FROM CURRENT_DATE) " +
-           "AND EXTRACT(YEAR FROM b.createdOn) = EXTRACT(YEAR FROM CURRENT_DATE) " +
-           "ORDER BY b.numViews DESC")
-    List<Book> findTopTenBooks();
+    @Query("SELECT b FROM Book b JOIN b.activeBooks ab WHERE EXTRACT(MONTH FROM b.createdOn) = " +
+        "EXTRACT(MONTH FROM CURRENT_DATE) AND EXTRACT(YEAR FROM b.createdOn) = EXTRACT(YEAR FROM CURRENT_DATE) ORDER BY b.numViews DESC")
+    List<Book> findTopTenBooks(PageRequest pageRequest);
+
+
     
 
 }
--- backend/src/main/java/com/bookadaisical/service/BookService.java
+++ backend/src/main/java/com/bookadaisical/service/BookService.java
@@ -2,17 +2,24 @@ package com.bookadaisical.service;
 
 import java.util.List;
 
+import org.hibernate.Hibernate;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.bookadaisical.model.Book;
 import com.bookadaisical.repository.BookRepository;
 import com.bookadaisical.service.interfaces.IBookService;
 
+import ch.qos.logback.classic.Logger;
+import jakarta.transaction.Transactional;
+
 @Service
 public class BookService implements IBookService {
 
     private final BookRepository bookRepository;
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(BookService.class);
 
     @Autowired
     public BookService(BookRepository bookRepository) {
@@ -20,8 +27,10 @@ public class BookService implements IBookService {
     }
 
     @Override
+    @Transactional
     public List<Book> getTopTenBooks() throws Exception {
-        List<Book> topTenMonthlyBooks = bookRepository.findTopTenBooks();
+        List<Book> topTenMonthlyBooks = bookRepository.findTopTenBooks(PageRequest.of(0, 10));
+
         return topTenMonthlyBooks;
     }
 }
