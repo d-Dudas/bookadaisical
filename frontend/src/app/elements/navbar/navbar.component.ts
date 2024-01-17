0b069863a03d1ff48ee2b9f9a5b47c8201215ef9
diana-gl <diana.gliga02@e-uvt.ro>
2024-01-17 07:46:49 +0200

    styled the components

--- frontend/src/app/elements/navbar/navbar.component.ts
+++ frontend/src/app/elements/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectIsAuthenticated, selectUser } from 'src/app/account-management/auth.state';
+import { ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,12 +11,30 @@ import { selectIsAuthenticated, selectUser } from 'src/app/account-management/au
 export class NavbarComponent {
   isAuthenticated: boolean = false;
   username: string = "";
+  isNavHidden = false;
+  private prevScrollPos = window.scrollY;
 
-  constructor(private store: Store){}
+  constructor(private store: Store, private el: ElementRef, private renderer: Renderer2){}
 
   ngOnInit()
   {
     this.store.select(selectIsAuthenticated).subscribe((is) => {this.isAuthenticated = is;});
     this.store.select(selectUser).subscribe((username) => {this.username = username!});
   }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const currentScrollPos = window.scrollY;
+
+    if (this.prevScrollPos > currentScrollPos) {
+      this.isNavHidden = false;
+      this.renderer.removeClass(this.el.nativeElement.querySelector('.navbar-container'), 'hidden');
+    } else {
+      this.isNavHidden = true;
+      this.renderer.addClass(this.el.nativeElement.querySelector('.navbar-container'), 'hidden');
+    }
+
+    this.prevScrollPos = currentScrollPos;
+  }
+  
 }
